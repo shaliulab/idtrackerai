89b6ee2441c1624ada946f1eb7f73bdd877d8abe
fjhheras <fjhheras@gmail.com>
2017-06-30 15:10:44 +0100

    Making get_trajectories backwards compatible

--- restructure/get_trajectories.py
+++ restructure/get_trajectories.py
@@ -15,7 +15,9 @@ sys.path.append('IdTrackerDeep/tracker')
 from blob import ListOfBlobs
 
 BLOB_FILE_NAME = "blobs_collection.npy"
-TRAJECTORIES_FILE_NAME = "trajectories.npy"
+CENTROID_TRAJECTORIES_FILE_NAME = "centroid_trajectories.npy"
+NOSE_TRAJECTORIES_FILE_NAME = "nose_trajectories.npy"
+HEAD_TRAJECTORIES_FILE_NAME = "head_trajectories.npy"
 
 def produce_trajectories(file_name):
     list_of_blobs = ListOfBlobs.load(file_name)
@@ -27,15 +29,21 @@ def produce_trajectories(file_name):
     for frame_number, blobs_in_frame in enumerate(list_of_blobs.blobs_in_video):
         print(frame_number)
         for blob in blobs_in_frame:
-            if blob.identity is not None:
+            if (blob.identity is not None) and (blob.identity != 0): #If blob is not a jump and it is not a crossing
                 centroid_trajectories[frame_number, blob.identity-1, :] = blob.centroid
-                head_trajectories[frame_number, blob.identity-1, :] = blob.head_trajectories
-                nose_trajectories[frame_number, blob.identity-1, :] = blob.nose_trajectories
+                try:
+                    head_trajectories[frame_number, blob.identity-1, :] = blob.head_trajectories
+                    nose_trajectories[frame_number, blob.identity-1, :] = blob.nose_trajectories
+                except: #For compatibility with previous versions where portrait is a tuple
+                    head_trajectories[frame_number, blob.identity-1, :] = blob.portrait[2]
+                    nose_trajectories[frame_number, blob.identity-1, :] = blob.portrait[1]
+ 
     return {"centroid": centroid_trajectories, "nose": nose_trajectories, "head": head_trajectories}
 
 if __name__ == "__main__":
     trajectories = produce_trajectories(BLOB_FILE_NAME)
     #print("Size: ", trajectories.shape)
     #print(trajectories)
-    np.save(TRAJECTORIES_FILE_NAME, trajectories)
-
+    np.save(CENTROID_TRAJECTORIES_FILE_NAME, trajectories['centroid'])
+    np.save(NOSE_TRAJECTORIES_FILE_NAME, trajectories['nose'])
+    np.save(HEAD_TRAJECTORIES_FILE_NAME, trajectories['head'])
