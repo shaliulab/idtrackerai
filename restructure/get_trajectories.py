db77f0d1d9ff55c8999fe48efbbfdfcc16833152
fjhheras <fjhheras@gmail.com>
2017-07-27 14:12:27 +0100

    Improved get_trajectories. Not thoroughly tested.

--- restructure/get_trajectories.py
+++ restructure/get_trajectories.py
@@ -1,12 +1,18 @@
-#import os
-#from os.path import isdir, isfile
-import sys
-#import glob
-import numpy as np
-#import cPickle as pickle
+"""
+Usage: get_trajectories.py
 
-# Import third party libraries
-#import cv2
+Contains tools to process tracked ListOfBlobs
+and output trajectories as numpy files with dimensions:
+    
+    [Individual number  x  frame number  x  coordinate (x,y)]
+
+When a certain individual was not identified in the frame
+a NaN appears instead of the coordinates
+"""
+
+import sys
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter1d
 
 # Import application/library specifics
 sys.path.append('IdTrackerDeep/restructure')
@@ -14,36 +20,55 @@ sys.path.append('IdTrackerDeep/tracker')
 
 from blob import ListOfBlobs
 
-BLOB_FILE_NAME = "blobs_collection.npy"
-CENTROID_TRAJECTORIES_FILE_NAME = "centroid_trajectories.npy"
-NOSE_TRAJECTORIES_FILE_NAME = "nose_trajectories.npy"
-HEAD_TRAJECTORIES_FILE_NAME = "head_trajectories.npy"
+def smooth_trajectories(t, sigma = 1.5, truncate = 4.0, derivative = 0):
+    """Smooth trajectories (and maybe perform derivatives)
 
-def produce_trajectories(file_name):
-    list_of_blobs = ListOfBlobs.load(file_name)
-    number_of_frames = len(list_of_blobs.blobs_in_video) 
+    :param t: trajectories as np.array [Individual, frame, coordinates]
+    :param sigma: standard deviation of Gaussian kernel
+    :param truncate: truncate filter at this number of std. Increase this if performing derivatives
+    :param derivative: order of performed derivative (0: none, 1: first derivative ...)
+    :returns: smoothed (and maybe derived) trajectoroes as np.array
+    """
+    t = gaussian_filter1d(t, sigma=sigma, axis=1, truncate = truncate, order = derivative)
+    return t
+
+def produce_trajectories(blob_file):
+    """Produce trajectories from ListOfBlobs
+
+    :param blob_file: ListOfBlobs instance
+    :returns: A dictionary with np.array as values
+    """
+    list_of_blobs = ListOfBlobs.load(blob_file)
+    number_of_frames = len(list_of_blobs.blobs_in_video)
     number_of_animals = list_of_blobs.blobs_in_video[0][0].number_of_animals #TODO number_of_animals should be a member of list_of_blobs
-    centroid_trajectories = np.ones((number_of_frames, number_of_animals, 2))*np.NaN
-    nose_trajectories = np.ones((number_of_frames, number_of_animals, 2))*np.NaN
-    head_trajectories = np.ones((number_of_frames, number_of_animals, 2))*np.NaN
+
+    centroid_trajectories = np.ones((number_of_animals,number_of_frames, 2))*np.NaN
+    nose_trajectories = np.ones((number_of_animals,number_of_frames, 2))*np.NaN
+    head_trajectories = np.ones((number_of_animals, number_of_frames, 2))*np.NaN
+    
     for frame_number, blobs_in_frame in enumerate(list_of_blobs.blobs_in_video):
         print(frame_number)
         for blob in blobs_in_frame:
             if (blob.identity is not None) and (blob.identity != 0): #If blob is not a jump and it is not a crossing
-                centroid_trajectories[frame_number, blob.identity-1, :] = blob.centroid
+                centroid_trajectories[blob.identity-1, frame_number, :] = blob.centroid
                 try:
-                    head_trajectories[frame_number, blob.identity-1, :] = blob.head_trajectories
-                    nose_trajectories[frame_number, blob.identity-1, :] = blob.nose_trajectories
+                    head_trajectories[blob.identity-1, frame_number, :] = blob.head_coordinates
+                    nose_trajectories[blob.identity-1, frame_number, :] = blob.nose_coordinates
                 except: #For compatibility with previous versions where portrait is a tuple
-                    head_trajectories[frame_number, blob.identity-1, :] = blob.portrait[2]
-                    nose_trajectories[frame_number, blob.identity-1, :] = blob.portrait[1]
+                    print("Warning: you are using an old ListOfBlobs file")
+                    head_trajectories[blob.identity-1, frame_number, :] = blob.portrait[2]
+                    nose_trajectories[blob.identity-1, frame_number, :] = blob.portrait[1]
  
     return {"centroid": centroid_trajectories, "nose": nose_trajectories, "head": head_trajectories}
 
 if __name__ == "__main__":
+    #SIMPLE USAGE EXAMPLE     
+    BLOB_FILE_NAME = "blobs_collection.npy"
     trajectories = produce_trajectories(BLOB_FILE_NAME)
-    #print("Size: ", trajectories.shape)
-    #print(trajectories)
-    np.save(CENTROID_TRAJECTORIES_FILE_NAME, trajectories['centroid'])
-    np.save(NOSE_TRAJECTORIES_FILE_NAME, trajectories['nose'])
-    np.save(HEAD_TRAJECTORIES_FILE_NAME, trajectories['head'])
+    for name in trajectories:
+        np.save(name + '_trajectories.npy', trajectories[name])
+        np.save(name + '_smooth_trajectories.npy', smooth_trajectories(trajectories[name]))
+        np.save(name + '_smooth_velocities.npy', smooth_trajectories(trajectories[name], derivative = 1))
+        np.save(name + '_smooth_accelerations.npy', smooth_trajectories(trajectories[name], derivative = 2))
+
+
