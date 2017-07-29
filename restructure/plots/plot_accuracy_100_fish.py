93eacb7c987cda201593eaad4b7e4b2f94180130
rhea <mattiagbergomi@gmail.com>
2017-07-29 11:19:43 +0100

    add computation to accuracy figure of 100fish

--- restructure/plots/plot_accuracy_100_fish.py
+++ restructure/plots/plot_accuracy_100_fish.py
@@ -16,37 +16,40 @@ from py_utils import get_spaced_colors_util
 
 if __name__ == '__main__':
 
-    results = pd.read_pickle('library/results_data_frame_all_2.pkl')
-    tests_names = [ 'correlated_images_DEF_aaa_CNN0_noPretrain_noAccum_100fish_3000frames_gaussian',
-                    'correlated_images_DEF_aaa_CNN0_noPretrain_Accum05_100fish_3000frames_gaussian',
-                    'correlated_images_DEF_aaa_CNN0_Pretrain_Accum05_100fish_3000frames_gaussian']
-    frames_per_fragment_conditions = list(results.frames_per_fragment.unique())
+    results = pd.read_pickle('library/results_data_frame_gamma_100fish_posterBehav2017_2.pkl')
+    tests_names = [ 'correlated_images_DEF_aaa_CNN0_noPretrain_noAccum_10fish_3000frames_gamma_portrait_duplications',
+                   'correlated_images_DEF_aaa_CNN0_noPretrain_Accum05_10fish_3000frames_gamma_portrait_duplications',
+                   'correlated_images_DEF_aaa_CNN0_Pretrain_Accum05_10fish_3000frames_gamma_portrait_duplications']
+    frames_per_fragment_conditions = [50.0, 250.0, 165.0, 848.5][::-1]
     num_frames_conditions = len(frames_per_fragment_conditions)
     num_repetitions = len(list(results.repetition.unique()))
 
-    acc = np.ones((3,5,10))*np.nan
+    acc = np.ones((3,4,5))
     for i, test_name in enumerate(tests_names):
-        results_test = results[results.test_name == test_name]
-        acc[i,:,:] = np.reshape(np.asarray(results_test.accuracy),(num_frames_conditions,num_repetitions))
+        for j, frames_per_fragment in enumerate(frames_per_fragment_conditions):
+            results_test = results[results.test_name == test_name]
+            results_test = results_test[results_test.frames_per_fragment == frames_per_fragment]
+            try:
+                acc[i,j,:] = np.asarray(results_test.accuracy)
+            except:
+                print(test_name, frames_per_fragment)
 
     plt.ion()
     fig, ax = plt.subplots(1)
     window = plt.get_current_fig_manager().window
     screen_y = window.winfo_screenheight()
     screen_x = window.winfo_screenwidth()
-    fig.set_size_inches((screen_x/100,screen_y/100))
+    fig.set_size_inches((screen_x*2/3/100,screen_y/100))
 
-    RGB_tuples = get_spaced_colors_util(num_frames_conditions, norm=True, black=False)
-    epsilon = [-0.01 , -0.005, 0., 0.005, 0.01]
-    epsilon = [-0.1 , -0.05, 0., 0.05, 0.1]
-    epsilon = [0. , 0., 0., 0., 0.]
+    # RGB_tuples = get_spaced_colors_util(num_frames_conditions, norm=True, black=False)
+    RGB_tuples = ['y', 'g', 'r', 'b'][::-1]
     for i, mean_frames in enumerate(frames_per_fragment_conditions):
         accuracies = np.squeeze(acc[:,i,:])
         print(accuracies)
-        acc_median = np.nanmedian(accuracies,axis = 1)
-        ax.plot(np.asarray([0,1,2]),acc_median,label = str(mean_frames), color = np.asarray(RGB_tuples[i]), linewidth = 2, marker = MARKERS[i])
-        for j in range(10):
-            ax.scatter(np.asarray([0,1,2])+epsilon[i], accuracies[:,j], color = np.asarray(RGB_tuples[i]), alpha = .3, marker = MARKERS[i])
+        acc_median = np.nanmean(accuracies,axis = 1)
+        ax.plot(np.asarray([0,1,2]),acc_median,label = str(mean_frames), color = RGB_tuples[i], linewidth = 2, marker = MARKERS[i])
+        for j in range(5):
+            ax.plot(np.asarray([0,1,2]), accuracies[:,j], color = RGB_tuples[i], alpha = .3, linewidth = 1, marker = MARKERS[i])
 
     ax.legend(title="mean number of \nframes in \nindividual fragments", fancybox=True)
     ax.set_xlabel('Algorithm protocol')
@@ -55,4 +58,4 @@ if __name__ == '__main__':
                                 'noPretrain\nAccum',
                                 'Pretrain\nAccum'])
     ax.set_ylim(0.75,1.01)
-    fig.savefig('100fish_library_tests_algorithm_protocols_portraits.pdf', transparent=True)
+    fig.savefig('100fish_library_tests_algorithm_protocols_portraits_gamma.pdf', transparent=True)
