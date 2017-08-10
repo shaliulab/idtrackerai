f3ffecb5d523696eb29528a7c9d47b2cb685db12
rhea <mattiagbergomi@gmail.com>
2017-08-10 12:02:58 +0100

    get_trajectories updated

--- restructure/get_trajectories.py
+++ restructure/get_trajectories.py
@@ -3,13 +3,13 @@ Usage: get_trajectories.py
 
 Contains tools to process tracked ListOfBlobs
 and output trajectories as numpy files with dimensions:
-    
+
     [Individual number  x  frame number  x  coordinate (x,y)]
 
 When a certain individual was not identified in the frame
 a NaN appears instead of the coordinates
 """
-
+import os
 import sys
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter1d
@@ -17,8 +17,10 @@ from scipy.ndimage.filters import gaussian_filter1d
 # Import application/library specifics
 sys.path.append('IdTrackerDeep/restructure')
 sys.path.append('IdTrackerDeep/tracker')
+sys.path.append('IdTrackerDeep/utils')
 from tqdm import tqdm
 from blob import ListOfBlobs
+from GUI_utils import selectDir
 
 
 def smooth_trajectories(t, sigma = 1.5, truncate = 4.0, derivative = 0):
@@ -46,7 +48,7 @@ def produce_trajectories(blob_file):
     centroid_trajectories = np.ones((number_of_animals,number_of_frames, 2))*np.NaN
     nose_trajectories = np.ones((number_of_animals,number_of_frames, 2))*np.NaN
     head_trajectories = np.ones((number_of_animals, number_of_frames, 2))*np.NaN
-    
+
     missing_head = False
     for frame_number, blobs_in_frame in enumerate(tqdm(list_of_blobs.blobs_in_video)):
         for blob in blobs_in_frame:
@@ -58,17 +60,24 @@ def produce_trajectories(blob_file):
                 except:
                     missing_head = True
     if missing_head:
-        return {"centroid": centroid_trajectories, "head": head_trajectories, "nose": nose_trajectories} 
+        return {"centroid": centroid_trajectories, "head": head_trajectories, "nose": nose_trajectories}
     return {"centroid": centroid_trajectories}
 
 if __name__ == "__main__":
-    #SIMPLE USAGE EXAMPLE     
-    BLOB_FILE_NAME = "blobs_collection.npy"
-    trajectories = produce_trajectories(BLOB_FILE_NAME)
+    # #SIMPLE USAGE EXAMPLE
+    # BLOB_FILE_NAME = "blobs_collection.npy"
+    session_path = selectDir('./') #select path to video
+    video_path = os.path.join(session_path,'video_object.npy')
+    print("loading video object...")
+    video = np.load(video_path).item(0)
+    blobs_path = video.blobs_path
+    trajectories_folder = os.path.join(video._session_folder,'trajectories')
+    if not os.path.isdir(trajectories_folder):
+        print("Creating trajectories folder...")
+        os.makedirs(trajectories_folder)
+    trajectories = produce_trajectories(blobs_path)
     for name in trajectories:
-        np.save(name + '_trajectories.npy', trajectories[name])
-        np.save(name + '_smooth_trajectories.npy', smooth_trajectories(trajectories[name]))
-        np.save(name + '_smooth_velocities.npy', smooth_trajectories(trajectories[name], derivative = 1))
-        np.save(name + '_smooth_accelerations.npy', smooth_trajectories(trajectories[name], derivative = 2))
-
-
+        np.save(os.path.join(trajectories_folder, name + '_trajectories.npy'), trajectories[name])
+        np.save(os.path.join(trajectories_folder, name + '_smooth_trajectories.npy'), smooth_trajectories(trajectories[name]))
+        np.save(os.path.join(trajectories_folder, name + '_smooth_velocities.npy'), smooth_trajectories(trajectories[name], derivative = 1))
+        np.save(os.path.join(trajectories_folder,name + '_smooth_accelerations.npy'), smooth_trajectories(trajectories[name], derivative = 2))
