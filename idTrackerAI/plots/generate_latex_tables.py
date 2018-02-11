4524a4f2d7abcbf693b69c3a2b0796e7c4093425
chronos <paco.romero.ferrero@gmail.com>
2018-02-11 12:16:24 +0000

    improve generate_latex_tables

--- idTrackerAI/plots/generate_latex_tables.py
+++ idTrackerAI/plots/generate_latex_tables.py
@@ -36,10 +36,11 @@ if __name__ == '__main__':
 
     tracked_videos_data_frame = pd.read_pickle(path_to_tracked_videos_data_frame)
 
-    columns_to_add = ['video_title',
+    columns_to_include = ['video_title',
                         'video_length_min',
                         'frame_rate',
                         'percentage_of_video_validated',
+                        'number_of_crossing_fragments_in_validated_part',
                         'mean_area_in_pixels',
                         'accuracy_identification_and_interpolation',
                         'accuracy_in_accumulation',
@@ -52,6 +53,7 @@ if __name__ == '__main__':
                 'Length (min)',
                 'Frame rate (fps)',
                 'perc. of video reviewed',
+                'Number of crossings',
                 'Pixels per animal',
                 'Accuracy',
                 'Accuracy in accumulation and identification',
@@ -64,6 +66,7 @@ if __name__ == '__main__':
                     f_time,
                     f_integer,
                     f_float,
+                    f_integer,
                     f_area,
                     f_accuracy,
                     f_accuracy,
@@ -72,16 +75,25 @@ if __name__ == '__main__':
                     f_percentage,
                     f_percentage]
 
-    ### short videos
-    smaller_groups_videos_data_frame = tracked_videos_data_frame[tracked_videos_data_frame.number_of_animals <= 35]
-    new_smaller = smaller_groups_videos_data_frame[columns_to_add].copy()
-    new_smaller.columns = new_columns_names
-    ### larger videos
-    larger_groups_videos_data_frame = tracked_videos_data_frame[tracked_videos_data_frame.number_of_animals > 35]
-    new_larger = larger_groups_videos_data_frame[columns_to_add].copy()
-    new_larger.columns = new_columns_names
+    def write_latex_table_for_subset_dataframe(data_frame, columns_to_include, new_columns_names, formatters, subset_condition, subtable_name):
+        assert len(columns_to_include) == len(new_columns_names) == len(formatters)
 
-    with open('./plots/smaller_group_size.tex','w') as tf:
-        tf.write(new_smaller.to_latex(index = False, formatters = formatters))
-    with open('./plots/larger_group_size.tex','w') as tf:
-        tf.write(new_larger.to_latex(index = False, formatters = formatters))
+        subset_data_frame = data_frame[subset_condition]
+        subset_data_frame = subset_data_frame[columns_to_include].copy()
+        subset_data_frame.columns = new_columns_names
+        with open('./plots/' + subtable_name + '.tex','w') as file:
+            file.write(subset_data_frame.to_latex(index = False, formatters = formatters))
+
+    ### smaller groups videos
+    print("generating smaller groups table")
+    condition = [x and not y for (x,y) in zip(list(tracked_videos_data_frame.number_of_animals <= 35), list(tracked_videos_data_frame.idTracker_video))]
+    # condition = tracked_videos_data_frame.number_of_animals <= 35 and not tracked_videos_data_frame.idTracker_video
+    write_latex_table_for_subset_dataframe(tracked_videos_data_frame, columns_to_include, new_columns_names, formatters, condition, 'smaller_group_sizes_table')
+    ### larger groups videos
+    print("generating larger groups videos table")
+    condition = [x and not y for (x,y) in zip(list(tracked_videos_data_frame.number_of_animals > 35), list(tracked_videos_data_frame.idTracker_video))]
+    write_latex_table_for_subset_dataframe(tracked_videos_data_frame, columns_to_include, new_columns_names, formatters, condition, 'larger_group_sizes_table')
+    ### idTracker videos
+    print("generating idTracker videos table")
+    condition = [bool(x) for x in tracked_videos_data_frame.idTracker_video]
+    write_latex_table_for_subset_dataframe(tracked_videos_data_frame, columns_to_include, new_columns_names, formatters, condition, 'idTracker_videos_table')
