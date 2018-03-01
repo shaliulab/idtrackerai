351739aa96bebc26d542407e3525fcdc64fe88cf
Francisco Romero <francisco.romero@neuro.fchampalimaud.org>
2018-03-01 12:19:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ conda env create -f environment-mint18.1.yml
 source activate idtrackerai-environment
 git clone https://gitlab.com/polavieja_lab/idtrackerai.git
 pip install idtrackerai/.
-garden install matoplotlib
 chmod +x idtrackerai/run.sh
+garden install matoplotlib
+
