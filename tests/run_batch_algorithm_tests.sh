6fa5b57eaff9da7a888a2d1c49592e6093e2c280
chronos2 <francisco.romero@neuro.fchampalimaud.org>
2018-06-08 08:59:28 +0100

    delete folder plots and tests, moved to idtrackerai_scripts repository

--- tests/run_batch_algorithm_tests.sh
+++
@@ -1,4 +0,0 @@
-python library/tests_data_frame.py
-for nt in 12 13 14 15 16 17; do
-	python run_algorithm_test.py 0 $nt
-done
--- tests/run_batch_network_tests.sh
+++
@@ -1,4 +0,0 @@
-python library/tests_data_frame.py
-for nt in 0; do
-	python run_network_test.py 0 $nt
-done
