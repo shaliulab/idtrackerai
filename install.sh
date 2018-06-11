106063983141bf56dabebc00f83e4d33a6ea1bea
chronos2 <francisco.romero@neuro.fchampalimaud.org>
2018-06-11 13:51:15 +0100

    update install.sh to version 1.0.2-alpha

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-wget https://gitlab.com/polavieja_lab/idtrackerai/raw/1.0.1-alpha/env-mint18.3-tf1.7-ocv2.13-kivy1.9.yml
+wget https://gitlab.com/polavieja_lab/idtrackerai/raw/1.0.2-alpha/env-mint18.3-tf1.7-ocv2.13-kivy1.9.yml
 conda env create -f env-mint18.3-tf1.7-ocv2.13-kivy1.9.yml
 source activate idtrackerai-environment
 git clone https://gitlab.com/polavieja_lab/idtrackerai.git
