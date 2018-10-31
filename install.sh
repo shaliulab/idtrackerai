2ba9d1e786fb9f863fa5a2ea2bb2388258965939
pacorofe <paco.romero.ferrero@gmail.com>
2018-10-31 18:58:26 +0000

    unpdate install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-wget https://gitlab.com/polavieja_lab/idtrackerai/raw/2.0.0-alpha/env-mint18.3-tf1.2-ocv2.13-kivy1.9.yml
+wget https://gitlab.com/polavieja_lab/idtrackerai/raw/2.0.0-alpha/env-mint18.3-tf1.9-ocv3.4.2-kivy1.10.yml
 conda env create -f env-mint18.3-tf1.9-ocv3.4.2-kivy1.10.yml
-source activate idtrackerai_py3
+source activate idtrackerai-env
 git clone https://gitlab.com/polavieja_lab/idtrackerai.git
 pip install idtrackerai/.
 garden install matplotlib
