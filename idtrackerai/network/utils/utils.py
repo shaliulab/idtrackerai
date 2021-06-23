2d67e5eae24b7ba8310cf5db056aefbbd898d1a9
Paco Romero Ferrero <paco.romero.ferrero@gmail.com>
2021-06-23 17:14:36 +0200

    xavier initialization right call

--- idtrackerai/network/utils/utils.py
+++ idtrackerai/network/utils/utils.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 
 def weights_xavier_init(m):
     if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
-        nn.init.xavier_uniform(m.weight.data)
+        nn.init.xavier_uniform_(m.weight.data)
 
 
 def fc_weights_reinit(m):
     if isinstance(m, nn.Linear):
-        nn.init.xavier_uniform(m.weight.data)
+        nn.init.xavier_uniform_(m.weight.data)
