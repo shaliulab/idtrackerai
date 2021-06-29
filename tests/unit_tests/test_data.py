f8d35bf8650abe8223e916484a0b31f10a29d27b
Paco Romero Ferrero <paco.romero.ferrero@gmail.com>
2021-06-29 15:30:08 +0200

    data inside of package

--- tests/unit_tests/test_data.py
+++ tests/unit_tests/test_data.py
@@ -1,7 +1,7 @@
 import os
 import idtrackerai.constants as cons
 
-DATA_FOLDER = os.path.join(os.path.dirname(cons.IDTRACKERAI_FOLDER), "data")
+DATA_FOLDER = os.path.join(cons.IDTRACKERAI_FOLDER, "data")
 TEST_VIDEO_COMPRESSED_PATH = os.path.join(
     DATA_FOLDER,
     "example_video_compressed",
--- tests/unit_tests/test_segmentation_utils.py
+++ tests/unit_tests/test_segmentation_utils.py
@@ -14,7 +14,7 @@ import idtrackerai.constants as cons
 
 import numpy as np
 
-DATA_FOLDER = os.path.join(os.path.dirname(cons.IDTRACKERAI_FOLDER), "data")
+DATA_FOLDER = os.path.join(cons.IDTRACKERAI_FOLDER, "data")
 TEST_VIDEO_COMPRESSED_PATH = os.path.join(
     DATA_FOLDER,
     "example_video_compressed",
