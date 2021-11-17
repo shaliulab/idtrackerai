41e446f88614bd80ebf05e64d4cf25fe6efd6ae2
antortjim <ntoniohu@gmail.com>
2021-11-17 23:36:13 +0000

    Fix typos

--- idtrackerai/utils/idtrackerai_loop.py
+++ idtrackerai/utils/idtrackerai_loop.py
@@ -28,7 +28,7 @@ def build_idtrackerai_call(experiment_folder, chunk, config_file):
 
 
 def write_jobfile(idtrackerai_call, jobfile, chunk="000000", environment="idtrackerai", knowledge_transfer=None):
-    
+
     lines = [
         "#! /bin/bash",
         f"source ~/.bashrc_conda && conda activate {environment}",
@@ -40,24 +40,24 @@ def write_jobfile(idtrackerai_call, jobfile, chunk="000000", environment="idtrac
         local_settings_py_backup = os.path.join(os.path.dirname(jobfile), f"session_{chunk}-local_settings.py")
         lines.append(f"cp local_settings.py {local_settings_py_backup}")
 
-    lines.append(idtrackerai_call)     
+    lines.append(idtrackerai_call)
 
     with open(jobfile, "w") as fh:
         for line in lines:
             fh.write(f"{line}\n")
-    
-    return    
+
+    return
 
 
 def build_qsub_call(experiment_folder, chunk, config_file, **kwargs):
-    
+
     # prepare the call to idtrackerai
     idtrackerai_call = build_idtrackerai_call(experiment_folder, chunk, config_file)
 
     # save the call together with a setup block into a script
-    jobfile = os.path.join(experiment_folder, f"session_{chunk}.sh")     
+    jobfile = os.path.join(experiment_folder, f"session_{chunk}.sh")
     write_jobfile(idtrackerai_call, jobfile, chunk=chunk, **kwargs)
-    
+
     # add the qsub flags
     output_file = os.path.join(experiment_folder, f"session_{chunk}_output.txt")
     error_file = os.path.join(experiment_folder, f"session_{chunk}_error.txt")
@@ -67,10 +67,10 @@ def build_qsub_call(experiment_folder, chunk, config_file, **kwargs):
 
 
 def run_one_loop(experiment_folder, chunk, config_file, **kwargs):
-    
+
     qsub_call = build_qsub_call(experiment_folder, chunk, config_file, **kwargs)
     process = subprocess.Popen(qsub_call, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
+    stdout, stderr = process.communicate()
     return stdout, stderr
 
 
@@ -78,14 +78,18 @@ def get_network_folder(datetime_str, i):
 
     session_folder = os.path.join(DATA_DIR, datetime_str, f"session_{str(i).zfill(6)}")
     folders_in_session = os.listdir(session_folder)
-    accumulation_folders = [os.path.join(session_folder, folder) for folder in folders_in_sessions if re.search("accumulation_[0-9]", folder)]
-    if len(accumulation_folders) == 0:
+    accum_folders = []
+    for folder in folders_in_session:
+        if re.search("accumulation_[0-9]", folder):
+            accum_folders.append(os.path.join(session_folder, folder))
+
+    if len(accum_folders) == 0:
         if i > 0:
             return get_network_folder(datetime_str, i-1)
         else:
             return None
     else:
-        last_network = sorted(accumulation_folders)[-1]
+        last_network = sorted(accum_folders)[-1]
         return last_network
 
 
@@ -94,7 +98,7 @@ def main(args=None):
     if args is None:
         ap = get_parser()
         args = ap.parse_args()
-    
+
     if args.side == "left":
         side_code = "_1"
     elif args.side == "right":
@@ -108,7 +112,7 @@ def main(args=None):
     for i in range(*args.interval, 1):
         chunk = str(i).zfill(6)
         if args.knowledge_transfer == "previous":
-            knowledge_transfer = get_network_folder(i-1)
+            knowledge_transfer = get_network_folder(args.datetime, i-1)
 
         elif os.path.exists(args.knowledge_transfer):
             knowledge_transfer=args.knowledge_transfer
