e0949d5e17613e9ba038e12ff487f986686f04e3
Antonio Ortega <ntoniohu@gmail.com>
2021-12-27 12:20:52 +0100

    Update videoimgstore

--- idtrackerai/video_imgstore.py
+++ idtrackerai/video_imgstore.py
@@ -9,14 +9,16 @@ logger = logging.getLogger(__name__)
 
 class VideoImgstore(Video):
 
-    def __init__(self, store_list, chunk):
+    def __init__(self, store, chunk=0):
 
-        self._stores = store_list
         self._chunk = chunk
-
         chunk_numbers = [chunk]
 
-        self._store = new_for_filename(store_list[0], chunk_numbers=chunk_numbers)
+        self._store = new_for_filename(
+            store,
+            chunk_numbers=chunk_numbers
+        )
+
         logger.info(f"Opening {self._store.current_video_path}")
         super().__init__(
             video_path = self._store.current_video_path,
