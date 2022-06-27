cf6f0a0ac6e7d76e97e462e4e21ddf1fbb40b007
antortjim <ntoniohu@gmail.com>
2022-06-27 16:42:28 +0000

    Update python-video-annotator

--- setup.py
+++ setup.py
@@ -112,8 +112,8 @@ setup(
         "gui": [
             IDTRACKERAI_APP_DEPENDENCY,
             "pyforms-gui==4.904.152",
-            "python-video-annotator==3.306",
-            "python-video-annotator-module-idtrackerai == 1.0.1a0",
+            "python-video-annotator-shaliulab==3.6.0",
+            "python-video-annotator-module-idtrackerai == 1.0.4",
         ],
         "gpu": ["torch", "torchvision"],
         "dev": ["pytest", "black", "sphinx", "numpydoc"],
