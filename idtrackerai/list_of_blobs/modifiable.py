5ce69a8a82eb346360027d7888afc4f212803353
antortjim <ntoniohu@gmail.com>
2023-04-18 12:51:25 +0200

    Clean up Modifiable and make it an ABC

--- idtrackerai/list_of_blobs/modifiable.py
+++ idtrackerai/list_of_blobs/modifiable.py
@@ -1,11 +1,12 @@
 import logging
 import copy
+from abc import ABC, abstractmethod
 
 
 logger = logging.getLogger(__name__)
 
-class Modifications:
-    
+class Modifications(ABC):
+
     @property
     def original_segmentation(self):
         self._original_segmentation = getattr(self, "_original_segmentation", {})
@@ -16,12 +17,12 @@ class Modifications:
     def modified_segmentation(self):
         self._modified_segmentation = getattr(self, "_modified_segmentation", {})
         return self._modified_segmentation
-    
+
     def apply_modification(self, frame_number, blobs_in_frame):
-        
+
         for blob in blobs_in_frame:
             blob.next, blob.previous = [], []
-            blob._bridge_start, blob._bridge_end = None, None            
+            blob._bridge_start, blob._bridge_end = None, None
 
 
         if all([not blob.modified for blob in self.blobs_in_video[frame_number]]):
@@ -31,20 +32,20 @@ class Modifications:
                 blob.next, blob.previous = [], []
                 blob._bridge_start, blob._bridge_end = None, None
                 original_blob = copy.deepcopy(blob)
-                original_blobs_in_frame.append(original_blob) 
+                original_blobs_in_frame.append(original_blob)
 
             self.original_segmentation[frame_number] = original_blobs_in_frame
-            
-        logger.debug(f"Applying modification for frame number {frame_number}")
+
+        logger.debug("Applying modification for frame number %s", frame_number)
         self.blobs_in_video[frame_number] = blobs_in_frame
         self.modified_segmentation[frame_number] = blobs_in_frame
 
     def toggle_modifications(self, value=True):
-        
+
         if value:
             for frame_number in self.modified_segmentation:
                 self.blobs_in_video[frame_number] = self.modified_segmentation[frame_number]
-                
+
         else:
             for frame_number in self.original_segmentation:
                 self.blobs_in_video[frame_number] = self.original_segmentation[frame_number]
